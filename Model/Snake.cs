ed501f9576547cd91a7fe340559eddf355ce81f2
juliaeles6 <juliaeles6@gmail.com>
2024-07-18 19:15:55 +0200

    Model almost done

--- Model/Snake.cs
+++ Model/Snake.cs
@@ -2,5 +2,77 @@
 {
     public class Snake
     {
+        private Tuple<int, int> _head;
+        private List<Tuple<int, int>> _tail;
+        private int _direction;
+        private Tuple<int, int>[] _aux = { new Tuple<int, int>(0, -1), new Tuple<int, int>(-1, 0), new Tuple<int, int>(0, 1), new Tuple<int, int>(1, 0) };
+
+        public event EventHandler GameOver;
+
+        public Tuple<int, int> Head
+        {
+            get
+            {
+                return _head;
+            }
+            private set
+            {
+
+            }
+        }
+
+        public List<Tuple<int, int>> Tail
+        {
+            get
+            {
+                return _tail;
+            }
+            private set
+            {
+
+            }
+        }
+
+        public Snake(Tuple<int, int> head)
+        {
+            _head = head;
+            _direction = 0;
+            _tail = [Utils.Add(_head, new Tuple<int, int>(0, 2)), Utils.Add(_head, new Tuple<int, int>(0, 1))];
+        }
+
+        public void Move(bool grow)
+        {
+            _tail.Add(_head);
+
+            if (!grow)
+            {
+                _tail.RemoveAt(0);
+            }
+
+            _head = Utils.Add(_head, _aux[_direction]);
+
+            if (_tail.Any(item => item.Equals(_head)))
+            {
+                GameOver.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        private void TurnLeft()
+        {
+            _direction = _direction - 1;
+            if (_direction < 0)
+            {
+                _direction = 3;
+            }
+        }
+
+        private void TurnRight()
+        {
+            _direction = _direction + 1;
+            if (_direction > 3)
+            {
+                _direction = 0;
+            }
+        }
     }
 }
--- Model/Table.cs
+++ Model/Table.cs
@@ -2,5 +2,51 @@
 {
     public class Table
     {
+        private Snake _snake;
+        private Tuple<int, int> _apple;
+        private int _score;
+        private Random _random;
+
+        public event EventHandler<int> GameOver;
+
+        public Table()
+        {
+            _random = new Random();
+            // _random.Next(0, 10);
+            _apple = new Tuple<int, int>(_random.Next(0, 10), _random.Next(0, 10));
+            _snake = new Snake(new Tuple<int, int>(_random.Next(0, 10), _random.Next(0, 10)));
+            _snake.GameOver += OnGameOver;
+
+            _score = 0;
+        }
+
+        public void MoveSnake()
+        {
+            //_snake.Move();
+            if (_snake.Head.Equals(_apple))
+            {
+                _score = _score + 1;
+                _snake.Move(true);
+                GenerateApple();
+            }
+            else
+            {
+                _snake.Move(false);
+            }
+        }
+
+        private void GenerateApple()
+        {
+            _apple = new Tuple<int, int>(_random.Next(0, 10), _random.Next(0, 10));
+            while (_apple.Equals(_snake.Head) || _snake.Tail.Any(item => item.Equals(_apple)))
+            {
+                _apple = new Tuple<int, int>(_random.Next(0, 10), _random.Next(0, 10));
+            }
+        }
+
+        private void OnGameOver(object? sender, EventArgs e)
+        {
+            GameOver.Invoke(this, _score);
+        }
     }
 }
